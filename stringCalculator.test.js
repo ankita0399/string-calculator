12fec369b01e54028d20fce51e228bf679fac31f
Ankita Kumari <ankitak0399@gmail.com>
2025-09-16 11:27:24 +0530

    Test: sum any amount of comma-separated numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -12,3 +12,8 @@ test('returns sum for two comma-separated numbers', () => {
   expect(add("1,5")).toBe(6);
 });
 
+test('returns sum for any amount of numbers', () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
+
+
