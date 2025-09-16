4d28d3fb8103072b39dece4f7d34d3c907a7982c
Ankita Kumari <ankitak0399@gmail.com>
2025-09-16 11:30:56 +0530

    Test: support custom delimiter definition

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -20,5 +20,10 @@ test('handles new line as delimiter along with commas', () => {
   expect(add("1\n2,3")).toBe(6);
 });
 
+test('supports custom delimiters specified at start', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
+
+
 
 
