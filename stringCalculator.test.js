facde7491e59d225fa1dbdded03bf1b29718d692
Ankita Kumari <ankitak0399@gmail.com>
2025-09-16 11:22:16 +0530

    Test: return number itself for single input

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require('./stringCalculator');
 test('returns 0 for empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number itself for single input', () => {
+  expect(add("1")).toBe(1);
+});
