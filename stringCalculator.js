55e46d51da2ec0835650530f768f4ec39616d728
Ankita Kumari <ankitak0399@gmail.com>
2025-09-16 11:30:10 +0530

    Impl: split by comma or newline using regex

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  const parts = numbers.split(",");
+  const parts = numbers.split(/,|\n/); // regex split by , or \n
   return parts.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
