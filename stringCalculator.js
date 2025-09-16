f7f4a112a3470acb5cf0e8c73422c98c876902a6
Ankita Kumari <ankitak0399@gmail.com>
2025-09-16 11:31:26 +0530

    Impl: parse and apply custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,16 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  const parts = numbers.split(/,|\n/); // regex split by , or \n
+  let delimiter = /,|\n/; // default delimiters
+  let numStr = numbers;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2)); // extract delimiter
+    numStr = parts[1];
+  }
+
+  const parts = numStr.split(delimiter);
   return parts.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
